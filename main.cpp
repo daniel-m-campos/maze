ee20ece52273a809fd723ce6f6c3a4351ec16f2c
Daniel M Campos <daniel.m.campos@icloud.com>
2020-12-30 13:14:23 -0600

    Remove IndentWidth: 4

--- main.cpp
+++ main.cpp
@@ -12,50 +12,52 @@ using std::vector;
 enum class State { kEmpty, kObstacle };
 
 vector<State> ParseLine(const string &line) {
-	istringstream sline(line);
-	int n;
-	char c;
-	vector<State> row;
-	while (sline >> n >> c && c == ',') {
-		if (n == 0) {
-			row.push_back(State::kEmpty);
-		} else {
-			row.push_back(State::kObstacle);
-		}
-	}
-	return row;
+  istringstream sline(line);
+  int n;
+  char c;
+  vector<State> row;
+  while (sline >> n >> c && c == ',') {
+    if (n == 0) {
+      row.push_back(State::kEmpty);
+    } else {
+      row.push_back(State::kObstacle);
+    }
+  }
+  return row;
 }
 
 vector<vector<State>> ReadBoardFile(const string &path) {
-	ifstream my_file(path);
-	vector<vector<State>> board{};
-	if (my_file) {
-		string line;
-		while (getline(my_file, line)) {
-			vector<State> row = ParseLine(line);
-			board.push_back(row);
-		}
-	}
-	return board;
+  ifstream my_file(path);
+  vector<vector<State>> board{};
+  if (my_file) {
+    string line;
+    while (getline(my_file, line)) {
+      vector<State> row = ParseLine(line);
+      board.push_back(row);
+    }
+  }
+  return board;
 }
 
 string CellString(State cell) {
-	switch (cell) {
-		case State::kObstacle: return "⛰️   ";
-		default: return "0   ";
-	}
+  switch (cell) {
+    case State::kObstacle:
+      return "⛰️   ";
+    default:
+      return "0   ";
+  }
 }
 
 void PrintBoard(const vector<vector<State>> &board) {
-	for (const auto &i : board) {
-		for (auto j : i) {
-			cout << CellString(j);
-		}
-		cout << "\n";
-	}
+  for (const auto &i : board) {
+    for (auto j : i) {
+      cout << CellString(j);
+    }
+    cout << "\n";
+  }
 }
 
 int main() {
-	auto board = ReadBoardFile("../1.board");
-	PrintBoard(board);
+  auto board = ReadBoardFile("../1.board");
+  PrintBoard(board);
 }
