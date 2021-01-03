b25eeef238dfefd6be4a9d68f50a7a3a0785c663
Daniel M Campos <daniel.m.campos@icloud.com>
2021-01-03 15:53:30 -0600

    Add ExpandNeighbors test

--- src/path_finder.h
+++ src/path_finder.h
@@ -11,17 +11,16 @@ class PathFinder {
   Grid original_grid_;
   Grid grid_;
   const int deltas_[4][2]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-
-  void ExpandNeighbors(const Node &current, const Point &goal);
-  static void CellSort(std::vector<Node> *v);
-  Node Current();
-
  public:
+
   PathFinder();
   explicit PathFinder(Grid grid);
   void AddToOpen(Node node);
+  void ExpandNeighbors(const Node &current, const Point &goal);
   static int Heuristic(const Point &p1, const Point &p2);
   static bool Compare(Node a, Node b);
+  static void CellSort(std::vector<Node> *v);
+  Node Current();
   Grid Search(Point start, Point goal);
   std::vector<Node> &GetOpenNodes();
   Grid &GetGrid();
--- test/test.cpp
+++ test/test.cpp
@@ -118,62 +118,34 @@ TEST_CASE("IsValid Function Test", "[CheckValidCellTests]") {
   CHECK(grid.IsValid({4, 2}) == true);
 }
 
-// FIXME: Convert these to Catch tests
-// void TestExpandNeighbors() {
-//  std::cout << "----------------------------------------------------------"
-//            << "\n";
-//  std::cout << "ExpandNeighbors Function Test: ";
-//  vector<int> current{4, 2, 7, 3};
-//  int goal[2]{4, 5};
-//  vector<vector<int>> open{{4, 2, 7, 3}};
-//  vector<vector<int>> solution_open = open;
-//  solution_open.push_back(vector<int>{3, 2, 8, 4});
-//  solution_open.push_back(vector<int>{4, 3, 8, 2});
-//  vector<vector<State>> grid{{State::kClosed, State::kObstacle, State::kEmpty,
-//                              State::kEmpty, State::kEmpty, State::kEmpty},
-//                             {State::kClosed, State::kObstacle, State::kEmpty,
-//                              State::kEmpty, State::kEmpty, State::kEmpty},
-//                             {State::kClosed, State::kObstacle, State::kEmpty,
-//                              State::kEmpty, State::kEmpty, State::kEmpty},
-//                             {State::kClosed, State::kObstacle, State::kEmpty,
-//                              State::kEmpty, State::kEmpty, State::kEmpty},
-//                             {State::kClosed, State::kClosed, State::kEmpty,
-//                              State::kEmpty, State::kObstacle,
-//                              State::kEmpty}};
-//  vector<vector<State>> solution_grid = grid;
-//  solution_grid[3][2] = State::kClosed;
-//  solution_grid[4][3] = State::kClosed;
-//  ExpandNeighbors(current, goal, open, grid);
-//  CellSort(&open);
-//  CellSort(&solution_open);
-//  if (open != solution_open) {
-//    std::cout << "failed"
-//              << "\n";
-//    std::cout << "\n";
-//    std::cout << "Your open list is: "
-//              << "\n";
-//    PrintVectorOfVectors(open);
-//    std::cout << "Solution open list is: "
-//              << "\n";
-//    PrintVectorOfVectors(solution_open);
-//    std::cout << "\n";
-//  } else if (grid != solution_grid) {
-//    std::cout << "failed"
-//              << "\n";
-//    std::cout << "\n";
-//    std::cout << "Your grid is: "
-//              << "\n";
-//    PrintVectorOfVectors(grid);
-//    std::cout << "\n";
-//    std::cout << "Solution grid is: "
-//              << "\n";
-//    PrintVectorOfVectors(solution_grid);
-//    std::cout << "\n";
-//  } else {
-//    std::cout << "passed"
-//              << "\n";
-//  }
-//  std::cout << "----------------------------------------------------------"
-//            << "\n";
-//  return;
-//}
+TEST_CASE("ExpandNeighbors Function Test", "[ExpandNeighborsTests]") {
+  Node current{4, 2, 7, 3};
+  auto grid = Grid({{State::kClosed, State::kObstacle, State::kEmpty,
+                     State::kEmpty, State::kEmpty, State::kEmpty},
+                    {State::kClosed, State::kObstacle, State::kEmpty,
+                     State::kEmpty, State::kEmpty, State::kEmpty},
+                    {State::kClosed, State::kObstacle, State::kEmpty,
+                     State::kEmpty, State::kEmpty, State::kEmpty},
+                    {State::kClosed, State::kObstacle, State::kEmpty,
+                     State::kEmpty, State::kEmpty, State::kEmpty},
+                    {State::kClosed, State::kClosed, State::kEmpty,
+                     State::kEmpty, State::kObstacle, State::kEmpty}});
+  auto expected_grid = grid;
+  expected_grid.Close(current.point);
+  expected_grid.Close({3, 2});
+  expected_grid.Close({4, 3});
+
+  vector<Node> open{current};
+  auto expected_open = open;
+  expected_open.push_back({3, 2, 8, 4});
+  expected_open.push_back({4, 3, 8, 2});
+
+  PathFinder path_finder(grid);
+  path_finder.AddToOpen(current);
+  path_finder.ExpandNeighbors(current, {4, 5});
+
+  SECTION("Open nodes are correct") {
+    CHECK(path_finder.GetOpenNodes() == expected_open);
+  }
+  SECTION("Grid is correct") { CHECK(path_finder.GetGrid() == expected_grid); }
+}
